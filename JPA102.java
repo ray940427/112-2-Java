a22e51bc1abb915259373e9b47d15c1c4aec0e4f
ray940427 <161407743+ray940427@users.noreply.github.com>
2024-03-01 00:20:53 +0800

    Add files via upload

---
+++ JPA102.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class JPA102 {
+    public static void main(String[]args){
+        Scanner scn=new Scanner(System.in);
+        System.out.print("Please input:");
+        int k=scn.nextInt();
+        
+        System.out.print(k*2.20462+" kg");
+
+    }
+
+}
