a6d82312c9c4a8eca71ce3400641ce3f5f9f2632
ray940427 <161407743+ray940427@users.noreply.github.com>
2024-03-03 19:55:17 +0800

    Add files via upload

---
+++ JPA107.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class JPA107{
+public static void main(String[] args) {
+    Scanner scn=new Scanner(System.in);
+    System.out.print("");
+    int a=scn.nextInt();
+    int b=scn.nextInt();
+    int c=scn.nextInt();
+    int k=a+2*b+3*c;
+    int s=10+2*b+3*c;
+    System.out.println("The basketball grade is "+k);
+    System.out.println("The baseball grade is "+s);
+}
+}
